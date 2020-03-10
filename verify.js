0d7b7231fad227ee189b4e82289ff482f082da77
vraj72 <virajtandel72@gmail.com>
2020-03-10 13:22:09 +0530

    changes made

--- verify.js
+++ verify.js
@@ -11,8 +11,8 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: '***********@gmail.com',
-    pass: '********'
+    user: 'softmandev123@gmail.com',
+    pass: '#######'
   }
 });
 
@@ -20,8 +20,8 @@ var transporter = nodemailer.createTransport({
 var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
-  user: "*****",
-  password: "******",
+  user: "viraj",
+  password: "qwerty",
   database:"aadharDB"
 });
 
@@ -55,36 +55,38 @@ router.post('/verify',(req,res,next)=>{
 				     	if(results.length > 0){
 
 				     		console.log("number",results[0].phone_number," email",results[0].email)
-					     	res.status(200).send({
-				               "phone number":results[0].phone_number
-				               ,"email":results[0].email
-				             });
+					     	
 
 					     	const otp= getRandomInt();
 
 					     	
 
 
-					  //    	var mailOptions = {
-							//   from: 'AADHAR UIDAI',
-							//   to: results[0].email,
-							//   subject: 'Aadhar Authentication OTP',
-							//   text: 'OTP for Your Aadhar Authentication is '+otp
-							// };
+					     	var mailOptions = {
+							  from: 'AADHAR UIDAI',
+							  to: results[0].email,
+							  subject: 'Aadhar Authentication OTP',
+							  text: 'OTP for Your Aadhar Authentication is '+otp
+							};
 
-							// transporter.sendMail(mailOptions, function(error, info){
-							//   if (error) {
-							//     console.log(error);
-							//   } else {
-							//     console.log('Email sent: ' + info.response);
-							//   }
-							// }); 
+							transporter.sendMail(mailOptions, function(error, info){
+							  if (error) {
+							    console.log(error);
+							  } else {
+							    console.log('Email sent: ' + info.response);
+							  }
+							}); 
 
 							req.session.aadhar = req.body.aadharno;
 							req.session.otp = otp;
 							console.log("session ",req.session.aadhar,req.session.otp); 
 							req.session.cookie.expires = new Date(Date.now() + 900000)
 							req.session.save();
+							res.status(200).send({
+				               "phone number":results[0].phone_number
+				               ,"email":results[0].email
+				             });
+
 
 
 					     }
@@ -103,13 +105,13 @@ router.post('/verify',(req,res,next)=>{
 
 router.post('/verifyOTP',(req,res)=>{
 
-				console.log("verify OTP called "+req.body.aadharno+"req session aadhar"+req.session.aadhar+"req body otp"+req.body.OTP+"req session otp"+req.session.otp);
+				console.log("verify OTP called :"+req.body.aadharno+"  req session aadhar :"+req.session.aadhar+"  req body otp: "+req.body.OTP+"  req session otp : "+req.session.otp);
 				var usrn= Number(req.body.aadharno);
 
 				if(req.session.aadhar && req.session.otp == req.body.OTP){
 					console.log("session verified",req.session.aadhar,req.session.otp,req.session.cookie.maxAge);
 					
-					
+						req.session.destroy();
 						res.sendStatus(200);
 				}
 				else{
